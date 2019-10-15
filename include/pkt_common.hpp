23f055bec6d8fb7cc75591a8b1c1724874b4cf8e
Thibaut Stimpfling <thibaut.stimpfling@polymtl.ca>
2019-10-15 16:02:07 -0400

    Pre-conflict

--- include/pkt_common.hpp
+++ include/pkt_common.hpp
@@ -56,15 +56,10 @@ template <> struct hash<FiveTuple> {
 
 } // std
 
-<<<<<<< HEAD
-
-
-=======
 template<typename Message>
 struct ThreadCommunication {
     std::queue<Message> mqueue;     // the queue of messages
-    std::condition_variable mcond;  // the var iable communicating events
+    std::condition_variable mcond;  // the variable communicating events
     std::mutex mmutex;              // the locking mechanism
 };
->>>>>>> ffe2effa9c32a8e35a53b8e3a8d350e5db18aa2f
 #endif // __PKT_COMMON__
