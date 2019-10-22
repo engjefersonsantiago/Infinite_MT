d4a37efaa5e14ffae8b107e13a034b49d5aff986
Thibaut Stimpfling <thibaut.stimpfling@polymtl.ca>
2019-10-22 17:09:48 -0400

    Cleaning - Not completed yet.

--- include/policy.hpp
+++ include/policy.hpp
@@ -1,4 +1,6 @@
 
+#ifndef __POLICY__
+#define __POLICY__
 
 // Quels sont les composants communs a une politique de cache.
 
@@ -6,3 +8,21 @@
 // (lookup container controller) et l entree a retirer.
 // Interface avec le controller. 
 // Policy doit pouvoir s
+
+template< typename Stats_Value>
+class Policy{
+
+    public: 
+
+    virtual void update_stats(){
+        
+    }
+
+
+    private:
+
+
+
+}
+
+#endif
