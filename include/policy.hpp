f868a0c3587cd0910cdf63c6f0c4d3b11ece66d2
Thibaut Stimpfling <thibaut.stimpfling@polymtl.ca>
2019-11-04 15:39:26 -0500

    Policier template.

--- include/policy.hpp
+++ include/policy.hpp
@@ -5,7 +5,8 @@
 
 #include "pkt_common.hpp"
 #include "lookup_table.hpp"
-
+#include "stats.hpp"
+#include <random>
 // Quels sont les composants communs a une politique de cache.
 
 // Base sur un input (Cache Stats) + Event (Cache Miss), identifie l'entree a ajouter
@@ -15,77 +16,62 @@
 
 // Policier is specialized for a given politic. 
 // Currently assume that L1 cache enries are updated following a cache miss.
-// No stats 
+
 
 
 // Policier (Cache Management Unit)
-template< size_t Lookup_Size, typename Lookup_Value,typename Stats_Value>
+template< size_t Lookup_Size, typename Lookup_Value, size_t Stats_Size, typename Stats_Value>
 class Policier{
 
     public: 
     using cache_entry_t = std::pair<FiveTuple,Lookup_Value>;
-    using inter_thread_cpu_to_cache = ThreadCommunication<FiveTuple,Lookup_Value>;
 
-
-    Policier(inter_thread_digest_cpu& l1_to_cpu_comm, inter_thread_digest_cpu& l2_to_cpu_comm,
-    inter_thread_cpu_to_cache& cpu_to_l1_comm, inter_thread_cpu_to_cache& cpu_to_l2_comm ,
-    LookupTable<Lookup_Size, Lookup_Value>& lookup_table): l1_to_cpu_comm_{l1_to_cpu_comm}, l2_to_cpu_comm_{l2_to_cpu_comm},
-     cpu_to_l1_comm_{cpu_to_l1_comm}, cpu_to_l2_comm{cpu_to_l2_comm}, lookup_table_{lookup_table} {} 
+    Policier( LookupTable<Lookup_Size, Lookup_Value>& lookup_table,
+    ,      StatsContainer<Size, Stats_Value>& stats_table, five_tuple_vector_t& reference_five_tuple_vector): lookup_table_{lookup_table},  stats_table_{stats_table},
+     {} 
      
-    // Entry Management - Could also be entries.
-    virtual void promote_entry_to_L1_cache(){
 
-    }
-    virtual void remove_entry_from_L1_cache(){
-
-    };
-
-    virtual void promote_entry_to_L2_cache() 
-    }
-
-    virtual void remove_entry_from_L2_cache()
-
-    virtual void process_pkt_digest(){
-
-    }
-
-    
-    // Harvest Stats
-    // TODO: When stats should be harvestest?
-    virtual void get_L1_cache_stats(){
-
-    }
-
-    virtual void get_L2_cache_stats(){
-
-    }
-
-
-
-
+  
+    virtual cache_entry_t select_replacement_victim() =0; 
+ 
+ 
     private:
     // Lookup Table 
     LookupTable<Lookup_Size, Lookup_Value>& lookup_table_;
+
+    // Controller Five Tuple Vector
+    five_tuple_vector_t& reference_five_tuple_vector_;
 
     // Pkt Digest
     tuple_pkt_size_pair_t tuple_size_pair_;
 
     // Stats
-    // Container? 
+    CacheStats<Stats_Size, Stats_Value>& stats_table_; 
 
-    // Entry(ies) to remove / insert
-    cache_entry_t entry_to_remove_;
-    cache_entry_t entry_to_promote_;
-
-    // Inter-thread communication to CPU
-    inter_thread_digest_cpu& l1_to_cpu_comm_;
-    inter_thread_digest_cpu& l2_to_cpu_comm_;
-
-    // Inter-thread communication from CPU
-    inter_thread_cpu_to_cache& cpu_to_l1_comm_;
-    inter_thread_cpu_to_cache& cpu_to_l2_comm_;
-
-
+    // Policy Type - Info Purpose
+    CacheType cache_type;
 };
+
+template< size_t Lookup_Size, typename Lookup_Value, size_t Stats_Size, typename Stats_Value>
+class RandomPolicy final: public Policier<Lookup_Size,Lookup_Value,Stats_Size,Stats_Value>
+
+    private:
+        size_t entry_index_to_remove;
+        constexpr auto Table_Size = Stats_Size;
+
+        // Hide the random generation mechanism
+        size_t random_number_generation(){
+            std::random_device rd;
+            std::mt19937 gen(rd());
+            std::uniform_int_distribution<> dis(0, Table_Size);
+            return dis(gen);
+        }
+
+    public:
+        virtual cache_entry_t select_replacement_victim(){
+            return reference_five_tuple_vector_[random_number_generation()];
+        }
+
+
 
 #endif
